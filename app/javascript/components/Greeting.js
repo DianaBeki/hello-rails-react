e46bce5f5d6e5539cd26d1e1e91f6f0a48fa0560
Diana Beki <dianabeki54@gmail.com>
2023-11-01 09:41:29 +0300

    Update file

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchGreetings } from '../redux/greetings/greetingSlice';
+import { fetchGreetings } from '../redux/greetings/greetingsSlice';
 
 const Greeting = () => {
   const dispatch = useDispatch();
